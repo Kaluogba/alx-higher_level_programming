c5f5456af18a14c78ce9f1094298bbdc8315f485
Kaluogba <kaluogba1990@gmail.com>
2022-11-30 11:10:23 -0800

    My Python Commits

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for obj in c:
-        if obj.islower():
-            return True
-        else:
-            return False
+    if obj(c) >= 97 and obj(c) <=122:
+        return True
+    else:
+        return False
