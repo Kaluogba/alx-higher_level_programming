317b7732bd9aa056c3946d7835a4c507a3d51b5e
Kaluogba <kaluogba1990@gmail.com>
2022-11-30 11:03:38 -0800

    My Python Commits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for i in range(0, 9):
     for j in range(1, 10):
         if i >= j:
             continue
-        print("{:d}""{:d}" ", " .format(i, j), end ="")
+        print("{:d}""{:d}" ", ".format(i, j), end="")
