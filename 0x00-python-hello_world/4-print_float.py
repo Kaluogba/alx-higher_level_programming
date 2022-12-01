1535e9f78e5085629e2b8502d7a31babff4c931d
Kaluogba <kaluogba1990@gmail.com>
2022-11-30 19:28:42 -0800

    My Python Commits

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 number = 3.14159
-number_float = "{:.2f}".format(number) 
-print(f'Float: {number_float}') 
+number_float = "{:.2f}".format(number)
+print(f'Float: {number_float}')
