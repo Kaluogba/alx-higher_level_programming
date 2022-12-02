0cfd3f0ee69af0bb9f54222a249bbd651dd446b4
Kaluogba <kaluogba1990@gmail.com>
2022-12-01 19:27:42 -0800

    My Python Commits

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,5 +5,5 @@ from add_0 import add
 a = 1
 b = 2
 
-result = add(1, 2)
-print("{:d} " "+" " {:d}" " =" " {:d}".format(a, b, result))
+result = add(a, b)
+print("{:d} " "+" " {:d} " "=" " {:d}".format(a, b, result))
