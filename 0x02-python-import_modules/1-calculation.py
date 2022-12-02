e6216ab8d29b3d8aa7bcc681845436b077e2681d
Kaluogba <kaluogba1990@gmail.com>
2022-12-02 04:09:48 -0800

    My Python Commits

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     result_div = div(a, b)
 
     print("{:d} + {:d} = {:d}".format(a, b, result_add))
-    print("{:d} + {:d} = {:d}".format(a, b, result_sub))
-    print("{:d} + {:d} = {:d}".format(a, b, result_mul))
-    print("{:d} + {:d} = {:d}".format(a, b, result_div))
+    print("{:d} - {:d} = {:d}".format(a, b, result_sub))
+    print("{:d} * {:d} = {:d}".format(a, b, result_mul))
+    print("{:d} / {:d} = {:d}".format(a, b, result_div))
