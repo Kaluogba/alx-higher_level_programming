055d5b50e196745308dbc598650abb48f5c6a3d3
Kaluogba <kaluogba1990@gmail.com>
2022-12-30 15:09:22 -0800

    My Python Commits

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for alp in c:
-        if alp.islower():
-            return True
-        else:
-            return False
+    if ord(c) >= 97 and ord(c) <= 122:
+        return (True)
+    else:
+        return (False)
