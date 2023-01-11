42f63692d546a17ed4257f7021e115d534ab3ea6
Kaluogba <kaluogba1990@gmail.com>
2023-01-11 11:58:28 -0800

    My Python I/O commits

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A Module that contains a function that appends a string at the end of a text file (UTF8) and returns the number of characters added
+"""
+
+def append_write(filename="", text=""):
+    """Function that appends to a text file
+
+    Args:
+        filename: name of file to be appended
+        text:string or text to be appended
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
