e3e92aa99f675f738d9520087381c521a5746758
Kalu Ogba Kalu <kaluogba1990@gmail.com>
2023-08-30 05:51:09 -0700

    Python classes and objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""Class Square that defines a square."""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """A class to define a square."""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """Initialize the Square instance.
+
+        Args:
+            size (int): The size of the square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.__size = size
