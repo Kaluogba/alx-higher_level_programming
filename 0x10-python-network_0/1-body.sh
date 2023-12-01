d97b85b94c7842866467302d91386a7505eac72d
Kaluogba <kaluogba1990@gmail.com>
2023-12-01 03:43:47 -0800

    Python Network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sX GET $1 -L
+# Script that shows the response body redirecting /route_1
+curl -Ls "$1"
